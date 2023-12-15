390169c23caf8c31bed9168932d4e3f9e3f88255
neerajmanivarnan <neerajmanivarnan666@gmail.com>
2023-12-15 21:09:42 +0530

    Initial Commit to SpringBoot

--- src/main/java/com/springdemo/springdemo/SpringdemoApplication.java
+++ src/main/java/com/springdemo/springdemo/SpringdemoApplication.java
@@ -2,12 +2,17 @@ package com.springdemo.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringdemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
+
+		Alien obj = context.getBean(Alien.class);
+		obj.demo();
+
 	}
 
 }
